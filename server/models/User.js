43cc85feebbd1f1d551e1a2e4acf29795175cbfd
ananyaaa26 <ananya.tuli@outlook.com>
2026-03-26 15:31:23 +0530

    Add comprehensive Sequelize validations to User model

--- server/models/User.js
+++ server/models/User.js
@@ -8,31 +8,65 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true
     },
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
       validate: {
-        len: [2, 100]
+        len: {
+          args: [2, 100],
+          msg: "Name must be between 2 and 100 characters"
+        },
+        notEmpty: {
+          msg: "Name cannot be empty"
+        }
       }
     },
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(255),
       allowNull: false,
-      unique: true,
+      unique: {
+        msg: "Email already in use"
+      },
       validate: {
-        isEmail: true
+        isEmail: {
+          msg: "Must be a valid email address"
+        },
+        notEmpty: {
+          msg: "Email cannot be empty"
+        }
       }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 255],
+          msg: "Password must be at least 6 characters long"
+        },
+        notEmpty: {
+          msg: "Password cannot be empty"
+        }
+      }
     },
     role: {
       type: DataTypes.ENUM('user', 'admin'),
-      defaultValue: 'user'
+      defaultValue: 'user',
+      validate: {
+        isIn: {
+          args: [['user', 'admin']],
+          msg: "Role must be either 'user' or 'admin'"
+        }
+      }
     }
   }, {
     tableName: 'users',
     timestamps: false,
+    indexes: [
+      {
+        unique: true,
+        fields: ['email']
+      }
+    ],
     hooks: {
       beforeCreate: async (user) => {
         if (user.password) {
